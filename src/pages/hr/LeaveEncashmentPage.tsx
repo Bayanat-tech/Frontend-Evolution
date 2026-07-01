b121720a3e7f833b45aace61ffd853e6a696fa37
Kush <tiwarikushmukesh+github@gmail.com>
2026-07-01 18:56:57 +0530

    employee and grade drop down salary increment page drop down company code added

--- src/pages/hr/LeaveEncashmentPage.tsx
+++ src/pages/hr/LeaveEncashmentPage.tsx
@@ -137,6 +137,7 @@ export function LeaveEncashmentPage() {
       code1: filters.divCode || undefined,
       code2: filters.divCode ? filters.deptCode || undefined : undefined,
       code3: filters.divCode ? filters.sectionCode || undefined : undefined,
+      code4: user?.company_code || '',
     })) as LookupRow[];
 
   const loadBalance = async (employeeId: string) => {
